dcbcf3e20ee564926fe6c159cc033d10d8f321cf
unknown <jens.osterberg@gmail.com>
2025-03-05 23:52:28 +0200

    Changed manifest theme color

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+## [1.0.0] - 05-03-2025
+
+### Release
+
+Version 1.0.0 is released, containing the bare-minimum features of the app:
+
+- User registration and login
+- Bet creation
+- Bid placement
+- Bet closure
--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -7,6 +7,7 @@ import { useUserContext } from '@/features/users/contexts/UserProvider';
 import { usePathname } from 'next/navigation';
 import { useMemo, useState } from 'react';
 import { IconButton } from '@mui/material';
+import pkg from 'betting_app/package.json';
 
 export function AppHeader() {
   const { user, status } = useUserContext();
@@ -35,12 +36,15 @@ export function AppHeader() {
   return (
     <Header>
       <div className='flex justify-between items-center w-full'>
-        <Link
-          className='flex gap-2 items-center index-link'
-          href='/'>
-          <Casino sx={{ transform: 'rotate(45deg)' }} />
-          <h1 className='text-xl font-semibold'>Wager Wave</h1>
-        </Link>
+        <div>
+          <Link
+            className='flex gap-2 items-center index-link'
+            href='/'>
+            <Casino sx={{ transform: 'rotate(45deg)' }} />
+            <h1 className='text-xl font-semibold'>Wager Wave</h1>
+          </Link>
+          <small>{pkg.version}</small>
+        </div>
 
         <nav className='gap-8 xs:hidden lg:flex'>{getLinks()}</nav>
         <div className='lg:hidden xs:block'>
@@ -51,7 +55,7 @@ export function AppHeader() {
             <nav
               onClick={() => setMenuOpen(false)}
               id='drop-down-menu'
-              className='absolute top-20 left-0 w-full flex flex-col items-end gap-8 z-10 bg-white p-4 border-b border-border animate-slide-down duration-500'>
+              className='absolute top-18 left-0 w-full flex flex-col items-end gap-8 z-10 bg-white p-4 border-b border-border animate-slide-down duration-500'>
               {getLinks()}
             </nav>
           )}
